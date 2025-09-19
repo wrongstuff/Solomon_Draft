5f54aea2b926731e2e34a6507dc19d745983b6bf
Alex Vacharat <alex.vacharat@gmail.com>
2025-09-19 00:10:21 -0500

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/Solomon_Draft/',
   plugins: [react()],
   resolve: {
     alias: {
